e7501504e8ed0f41ff2b5d6942f94e5842f86d9e
GustavoAG7201 <galonso004@outlook.com>
2023-11-17 10:34:16 -0600

    Segundos Avances 17-nov-2017
    Se agrego el nombre como comentario en el codigo

--- ProyectoFinal/LoginForm1.vb
+++ ProyectoFinal/LoginForm1.vb
@@ -1,14 +1,22 @@
+Imports System.Data.OleDb
+
 Public Class frmLogin
 
+    Private conexion As New OleDbConnection()
+
     Private Sub OK_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles OK.Click
+        'Alonso Gutierrez Gustavo'
         frmMenu.Show()
     End Sub
 
     Private Sub Cancel_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Cancel.Click
+        'Gustavo Alonso Gutierrez'
         Me.Close()
     End Sub
 
     Private Sub frmLogin_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Private conexion1
+        'Gustavo Alonso Gutierrez'
+
+
     End Sub
 End Class

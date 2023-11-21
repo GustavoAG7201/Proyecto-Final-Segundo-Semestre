a355ecf36aa1841475175f2468b3703838fe986c
GustavoAG7201 <galonso004@outlook.com>
2023-11-21 09:34:40 -0600

    Avances 21-nov-2023
    Se desconectó la anterior base de datos para pasarlo de la versión 16 a la 12.
    
    La conexión a la base de datos ya esta hecha

--- ProyectoFinal/LoginForm1.vb
+++ ProyectoFinal/LoginForm1.vb
@@ -2,11 +2,12 @@ Imports System.Data.OleDb
 
 Public Class frmLogin
 
-    Private conexion As New OleDbConnection("Provider=Microsoft.ACE.OLEDB.16.0;Data Source=" & Application.StartupPath & "\CBTis137.accdb")
+    Private conexion As New OleDbConnection("Provider = Microsoft.ACE.OLEDB.12.0;Data Source=" & Application.StartupPath & "\CBTis137_antigua.accdb")
 
     Private Sub OK_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles OK.Click
         'Alonso Gutierrez Gustavo'
         frmMenu.Show()
+
     End Sub
 
     Private Sub Cancel_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Cancel.Click
